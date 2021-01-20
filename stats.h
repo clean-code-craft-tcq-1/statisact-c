233ecbb07824a3ea38cfc87e33c58848a360e96d
Sudeep <sudeep.rp@gmail.com>
2021-01-20 14:52:19 +0530

    add alert

--- stats.h
+++ stats.h
@@ -2,7 +2,7 @@
 struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
-static void check_and_alert(float, alerter_funcptr[], Stats);
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
